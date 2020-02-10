fda1d6980974ecbfe233bc5f4650df0a6005742e
Lisa <lisa.michael@hotmail.com>
2020-02-09 21:38:23 -0500

    added the pseudo code app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
 import React, { Component } from "react";
 import './App.css';
-import logo from './logo.svg';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header"
-// import Card from "./components/Card";
 import Petrocard from "./components/Petrocard";
 import petro from "./petro.json"
 import Title from "./components/Title";
 import Wrapper from "./components/Wrapper";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  // Setting this.state.petro to the petro json array
   state = {
-    petro
+    petro,
+    clickedPetro: [],
+    score: 0,
+    highscore: 0
 
-  }
+  };
+
+  // function for when you click on an image 
+  imageClick = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+
+    // when an image is clicked , I need to check the id of the image clicked. 
+    this.state.score = this.setState({score: 1});
+
+    // if the image has already been clicked, set score to 0. Game over 
+
+    // if the image has not been clicked , I need to: 
+    
+    // check to see if the score exceeds the high score 
+    //scramble the images 
+    // increment the score by 1 
+   
+  };
 
   // Map over this.state.friends and render a PetroCard component for each Petroglyph object
   render() {
     return (
-     
-       
+
+
       <Wrapper>
- <Navbar></Navbar>
+        <Navbar></Navbar>
         <Title></Title>
         <Header></Header>
-        <Title></Title>
+
         {this.state.petro.map(petro => (
           <Petrocard
 
@@ -39,9 +57,9 @@ class App extends Component {
       </Wrapper>
     );
   }
+
+
 }
-
-
 
 
 
