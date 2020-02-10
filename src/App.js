7cfdbe50343d968104d77a27caebb72980a549f9
Lisa <lisa.michael@hotmail.com>
2020-02-09 21:47:21 -0500

    added the pseudo code app.js

--- src/App.js
+++ src/App.js
@@ -21,17 +21,22 @@ class App extends Component {
   imageClick = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
 
-    // when an image is clicked , I need to check the id of the image clicked. 
-    this.state.score = this.setState({score: 1});
+    // when an image is clicked , I need to check the clickedPetro arry for id of the image clicked. 
+
+    
 
     // if the image has already been clicked, set score to 0. Game over 
 
+
     // if the image has not been clicked , I need to: 
     
+    //incrment the score by  1 
+    this.state.score = this.setState({score: 1});
+    //add id to the clickedPetro(glyph) array 
     // check to see if the score exceeds the high score 
     //scramble the images 
-    // increment the score by 1 
-   
+    
+   // if the score = 12, the user wins, display message (not in hw specs)
   };
 
   // Map over this.state.friends and render a PetroCard component for each Petroglyph object
