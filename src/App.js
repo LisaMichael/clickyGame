bb634437207c1d37daedfca4f0688ce1aebc4cf2
Lisa <lisa.michael@hotmail.com>
2020-02-09 22:38:18 -0500

    added code after adding pseudo code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Title from "./components/Title";
 import Wrapper from "./components/Wrapper";
 
 class App extends Component {
-  // Setting this.state.petro to the petro json array
+  // Setting this.state.petro values
   state = {
     petro,
     clickedPetro: [],
@@ -18,26 +18,74 @@ class App extends Component {
   };
 
   // function for when you click on an image 
-  imageClick = id => {
+
+
     // Filter this.state.friends for friends with an id not equal to the id being removed
 
     // when an image is clicked , I need to check the clickedPetro arry for id of the image clicked. 
 
-    
+
 
     // if the image has already been clicked, set score to 0. Game over 
 
 
     // if the image has not been clicked , I need to: 
-    
+
     //incrment the score by  1 
-    this.state.score = this.setState({score: 1});
+
     //add id to the clickedPetro(glyph) array 
     // check to see if the score exceeds the high score 
     //scramble the images 
-    
-   // if the score = 12, the user wins, display message (not in hw specs)
-  };
+
+    // if the score = 12, the user wins, display message (not in hw specs)
+
+    imageClick = event => {
+      const currentPetroglyphs = event.target.alt;
+      const imagesAlreadyClicked =
+        this.state.clickedPetro.indexOf(currentPetroglyphs) > -1;
+
+
+      if (imagesAlreadyClicked) {
+        this.setState({
+          petro: this.state.petro.sort(function (a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedPetro: [],
+          score: 0
+        });
+        alert("Better luck next time! Try again?");
+
+
+      } else {
+        this.setState(
+          {
+            petro: this.state.petro.sort(function (a, b) {
+              return 0.5 - Math.random();
+            }),
+            clickedPetro: this.state.clickedPetro.concat(
+              currentPetroglyphs
+            ),
+            score: this.state.score + 1
+          },
+
+          () => {
+            if (this.state.score === 12) {
+              alert("Yay! You Win!");
+              this.setState({
+                petro: this.state.petro.sort(function (a, b) {
+                  return 0.5 - Math.random();
+                }),
+                clickedPetro: [],
+                score: 0
+              });
+            }
+          }
+        );
+      }
+    };
+
+
+  
 
   // Map over this.state.friends and render a PetroCard component for each Petroglyph object
   render() {
