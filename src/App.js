698c0f4d9cde17b8dfbbd02601b133402d0cfb25
Lisa <lisa.michael@hotmail.com>
2020-02-11 12:32:55 -0500

    Top score is now working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Petrocard from "./components/Petrocard/Petrocard";
 import petro from "./petro.json"
 // import Title from "./components/Title";
 import Wrapper from "./components/Wrapper";
+import { createTypeAnnotationBasedOnTypeof } from "@babel/types";
 
 class App extends Component {
   // Setting this.state.petro values
@@ -14,7 +15,7 @@ class App extends Component {
     clickedPetro: [],
     score: 0,
     status: "Click an image to begin!",
-    topscore: 1
+    topscore: 0
   };
 
 
@@ -27,7 +28,7 @@ class App extends Component {
 
     // when an image is clicked , I need to check the clickedPetro arry for id of the image clicked. 
 
-    
+
     // if already clicked, set score to zero, scramble images,  
     //reset already clicked array 
     if (imagesAlreadyClicked) {
@@ -37,6 +38,7 @@ class App extends Component {
         }),
         clickedPetro: [],
         score: 0,
+        topscore: 0,
         status: 'You guessed Incorrectly!'
       });
 
@@ -49,6 +51,11 @@ class App extends Component {
       //add id to the clickedPetro(glyph) array 
       //scramble the images 
       // check to see if the score exceeds the high score -NEED WORK
+
+      if(this.state.score > this.state.topscore){
+        this.state.topscore = this.state.score;
+      }
+
       this.setState(
         {
           petro: this.state.petro.sort(function (a, b) {
@@ -59,22 +66,25 @@ class App extends Component {
           ),
           status: "You guessed Correctly!",
           score: this.state.score + 1,
-          
 
+        topscore: this.state.topscore +1,
+ 
         },
+
+
 
         () => {
 
           // IF SCORE STATE =12 YOU WIN
           if (this.state.score === 12) {
-            alert("Yay! You Win!");
+            
             this.setState({
               petro: this.state.petro.sort(function (a, b) {
                 return 0.5 - Math.random();
               }),
               clickedPetro: [],
               score: 0,
-              status: 'AWESOME! YOUR A WINNER!'
+              status: "AWESOME! YOU'RE A WINNER!"
             });
           }
         }
@@ -93,7 +103,7 @@ class App extends Component {
         <Navbar
           status={this.state.status}
           score={this.state.score}
-topscore = {this.status.topscore}
+topscore={this.state.topscore}
         />
 
         <Header />
